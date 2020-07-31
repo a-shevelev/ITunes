3957eaded019ce8a9d7d37c8c16c807f165a7cdf
a-shevelev <60062720+a-shevelev@users.noreply.github.com>
2020-07-31 14:48:38 +0300

    Update videoPlayer.js
    videoPlayer

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,3 +1,78 @@
 export const videoPlayerInit = () => {
-    console.log('VideoInit');
+
+
+    const videoPlayer = document.querySelector('.video-player');
+    const videoButtonPlay = document.querySelector('.video-button__play');
+    const videoButtonStop = document.querySelector('.video-button__stop');
+    const videoProgress = document.querySelector('.video-progress');
+    const videoTimePassed = document.querySelector('.video-time__passed');
+    const videoTimeTotal = document.querySelector('.video-time__total');
+
+    
+    const toggleIcon = () => {
+        if (videoPlayer.paused) {
+            videoButtonPlay.classList.remove('fa-pause');
+            videoButtonPlay.classList.add('fa-play');
+        } else {
+            videoButtonPlay.classList.remove('fa-play');
+            videoButtonPlay.classList.add('fa-pause');
+        }
+    }
+
+    const togglePlay = () => {
+        if (videoPlayer.paused){
+        videoPlayer.play();
+        } else {
+            videoPlayer.pause();
+        }
+
+        toggleIcon();
+    };
+    const stopPlay = () => {
+        videoPlayer.pause();
+        videoPlayer.currentTime = 0;
+    };
+
+    //условие ? (условие верно) : (условие ложь)
+    /*const addZero = n => {
+        return n < 10 ? '0' + n : n;
+    };*/
+    const addZero = n => n < 10 ? '0' + n : n;
+    
+    videoPlayer.addEventListener('click', togglePlay);
+    videoButtonPlay.addEventListener('click', togglePlay);
+
+    videoPlayer.addEventListener('play', toggleIcon);
+    videoPlayer.addEventListener('pause', toggleIcon);
+
+    videoButtonStop.addEventListener('click', stopPlay);
+
+    
+    
+    videoPlayer.addEventListener('timeupdate', () => {
+        const currentTime = videoPlayer.currentTime;
+        const duration = videoPlayer.duration;
+
+        videoProgress.value = (currentTime / duration) * 100;
+
+        let minutePassed = Math.floor(currentTime / 60);
+        let secondPassed = Math.floor(currentTime % 60);
+
+        let minuteTotal = Math.floor(duration / 60);
+        let secondTotal = Math.floor(duration % 60);
+        
+        videoTimePassed.textContent = addZero(minutePassed) + ':' + addZero(secondPassed);
+        videoTimeTotal.textContent = addZero(minuteTotal) + ':' + addZero(secondTotal);
+    });
+
+    videoProgress.addEventListener('change', () =>{
+        const duration = videoPlayer.duration;
+        const value = videoProgress.value;
+
+        videoPlayer.currentTime = (value * duration) / 100;
+    });
+    
+
+
+    
 };
